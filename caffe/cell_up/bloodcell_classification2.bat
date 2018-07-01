586a0173605fe397c6a205684c93af9f65b0551a
wulei <532961641@qq.com>
2018-07-01 20:08:08 +0800

    尝试提交

--- caffe/cell_up/bloodcell_classification2.bat
+++ caffe/cell_up/bloodcell_classification2.bat
@@ -3,9 +3,9 @@ cd D:\caffeDev
 cd caffe-master
 
 set TOOLS=Build\x64\Release
-set EXAMPLE=examples\bloodcell
-set DATA=data\bloodcell
+set EXAMPLE=examples\cell
+set DATA=data\cell
 
-%TOOLS%\classification %EXAMPLE%\bloodcell_classification2.prototxt %EXAMPLE%\bloodcell_full2\_iter_10000.caffemodel.h5 %EXAMPLE%\bloodcell_train_mean.binaryproto %DATA%/class.txt %DATA%\test.jpeg
+%TOOLS%\classification %EXAMPLE%\Alexnet_resize_classification.prototxt %EXAMPLE%\cell_model_resize\_iter_10000.caffemodel %DATA%\cell_train_mean_lmdb1.binaryproto %DATA%/class.txt %DATA%\test.jpeg
 
 pause
